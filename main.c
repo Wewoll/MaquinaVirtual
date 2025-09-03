450b2b55db8c7eb9068e7af49220c66f66c8bc53
Lucas Paniagua <you@example.com>
2025-09-03 19:39:44 -0300

    Unos cambios en main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define RAM_SIZE (16 * 1024)    // 16 KiB
 #define REG_AMOUNT 32           // 32 registros
+#define SEG_AMOUNT 2            // 2 descriptores de segmentos
 
 #define CS_POS 0x00000000       // Primer entrada
 #define DS_POS 0x00010000       // Segunda entrada
@@ -18,27 +19,38 @@ typedef enum {
     CS = 26, DS
 } RegName;
 
-//Define de Instrucciones
+//Define de instrucciones
 typedef enum {
     SYS = 0x00, JMP, JZ, JP, JN, JNZ, JNP, JNN, NOT,
     STOP = 0x0F,
     MOV = 0x10, ADD, SUB, MUL, DIV, CMP, SHL, SHR, SAR, AND, OR, XOR, SWAP, LDL, LDH, RND
 } OpCode;
 
-//Tama�os usados
+//Tamanos usados
 typedef uint8_t Byte;
+typedef uint16_t DosBytes;
 typedef uint32_t Register;
 
+//Tabla de descriptores de segmentos
 typedef struct {
-    Byte     mem[RAM_SIZE];      // memoria RAM - 16384 celdas de 1 byte cada una
-    Register reg[REG_AMOUNT];    // bancos de registros - 32 registros de 4 bytes cada uno
-} MV;
+    DosBytes base;   // dirección lógica de inicio
+    DosBytes limit;  // tamaño del segmento en bytes
+} TableSeg;
 
-static void inicializacion(MV *mv) {
+//Maquina Virtual
+typedef struct {
+    Byte     mem[RAM_SIZE];       // 16 KiB de RAM
+    Register reg[REG_AMOUNT];     // 32 registros de 4 bytes
+    TableSeg seg[SEG_AMOUNT];     // tabla de segmentos: 0 = CS, 1 = DS
+} TMV;
+
+void inicializacion(TMV *mv) {
     unsigned int i;
 
-    for (i = 0; i < RAM_SIZE; i++) mv->mem[i] = 0;
-    for (i = 0; i < REG_AMOUNT; i++) mv->reg[i] = 0;
+    for (i = 0; i < RAM_SIZE; i++)
+        mv->mem[i] = 0;
+    for (i = 0; i < REG_AMOUNT; i++)
+        mv->reg[i] = 0;
 
     mv->reg[CS] = CS_POS;
     mv->reg[DS] = DS_POS;
@@ -46,7 +58,7 @@ static void inicializacion(MV *mv) {
 }
 
 int main() {
-    MV mv;
+    TMV mv;
     inicializacion(&mv);
     //TODO arranque de la lectura del archivo y su verificacion
 
