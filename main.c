f1e79447bf3728c10fb41b13d836f20ac8777ef2
Lucas Paniagua <you@example.com>
2025-09-03 21:01:50 -0300

    Lectura del archivo y validaciones

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #define CS_POS 0x00000000       // Primer entrada
 #define DS_POS 0x00010000       // Segunda entrada
 
+#define HEADER_RANGE 8
+
 //Define de registros
 typedef enum {
     LAR = 0, MAR, MBR,
@@ -28,13 +30,13 @@ typedef enum {
 
 //Tamanos usados
 typedef uint8_t Byte;
-typedef uint16_t DosBytes;
+typedef uint16_t TwoBytes;
 typedef uint32_t Register;
 
 //Tabla de descriptores de segmentos
 typedef struct {
-    DosBytes base;   // dirección lógica de inicio
-    DosBytes limit;  // tamaño del segmento en bytes
+    TwoBytes base;   // dirección lógica de inicio
+    TwoBytes size;   // tamaño del segmento en bytes
 } TableSeg;
 
 //Maquina Virtual
@@ -44,8 +46,80 @@ typedef struct {
     TableSeg seg[SEG_AMOUNT];     // tabla de segmentos: 0 = CS, 1 = DS
 } TMV;
 
-void inicializacion(TMV *mv) {
+void readFile(const char *filename, int *err, TMV *mv);
+void initialization(TMV *mv, TwoBytes codeSize);
+
+int main(int argc, char *argv[]) {
+    TMV mv;
+    int err = 0;
+
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s archivo.vmx [-d]\n", argv[0]);
+        err++;
+    }
+    else {
+        readFile(argv[1], &err, &mv)
+        if (err == 0) {
+            //executeProgram(&mv); Siguiente paso (va, eso y hacer las funciones de cada cosa)
+        }
+    }
+
+    return err;
+}
+
+void readFile(const char *filename, int *err, TMV *mv) {
+    FILE *arch;
+    Byte header[HEADER_RANGE];
+    TwoBytes codeSize;
+
+    arch = fopen(filename, "rb");
+    if (arch == NULL) {
+        perror("No se pudo abrir el archivo");
+        (*err)++;
+    }
+    else {
+        if (fread(header, 1, 8, arch) != HEADER_RANGE) {
+            fprintf(stderr, "Error: archivo muy corto\n");
+            (*err)++;
+        }
+        else {
+            if (memcmp(header, "VMX25", 5) != 0) {
+                fprintf(stderr, "Error: firma incorrecta\n");
+                (*err)++;
+            }
+            else {
+                if (header[5] != 1) {
+                    fprintf(stderr, "Error: version incorrecta (%d)\n", header[5]);
+                    (*err)++;
+                }
+                else {
+                    codeSize = (header[6] << 8) | header[7];
+                    if (codeSize >= RAM_SIZE) {
+                        fprintf(stderr, "Error: el codigo es demasiado grande (%u bytes)\n", codeSize);
+                        (*err)++;
+                    }
+                    else {
+                        initialization(mv, codeSize);
+                        if (fread(mv->mem, 1, codeSize, arch) != codeSize) {
+                            fprintf(stderr, "Error leyendo el codigo\n");
+                            (*err)++;
+                        }
+                    }
+                }
+            }
+        }
+
+        fclose(arch);
+    }
+}
+
+void initialization(TMV *mv, TwoBytes codeSize) {
     unsigned int i;
+
+    mv->seg[0].base = 0;
+    mv->seg[0].size = codeSize;
+    mv->seg[1].base = codeSize;
+    mv->seg[1].size = RAM_SIZE - codeSize;
 
     for (i = 0; i < RAM_SIZE; i++)
         mv->mem[i] = 0;
@@ -55,16 +129,4 @@ void inicializacion(TMV *mv) {
     mv->reg[CS] = CS_POS;
     mv->reg[DS] = DS_POS;
     mv->reg[IP] = mv->reg[CS];
-}
-
-int main() {
-    TMV mv;
-    inicializacion(&mv);
-    //TODO arranque de la lectura del archivo y su verificacion
-
-    //TODO while
-    //TODO fetch
-    //TODO decode + execute
-
-    return 0;
 }
